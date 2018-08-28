c73db5d8f51f597cc8db60c6882910e5f19d1bab
NikhilBansal1999 <nikhil.accent@gmail.com>
2018-08-28 19:24:33 +0530

    Trained for 26 epochs

--- train.py
+++ train.py
@@ -59,9 +59,9 @@ model.add(Dense(4500,activation='softmax'))
 sgd = keras.optimizers.SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False)
 model.compile(optimizer=sgd,loss='categorical_crossentropy',metrics=['accuracy'])
 
-#model.load_weights('my_model_weights.h5')
+model.load_weights('my_model_weights.h5')
 train_generator=data()
-model.fit_generator(generator=train_generator,steps_per_epoch=len(features_dict),epochs=1)
+model.fit_generator(generator=train_generator,steps_per_epoch=len(features_dict),epochs=5)
 model.save_weights('my_model_weights.h5')
 
 ep=open('epochs.txt','w')
